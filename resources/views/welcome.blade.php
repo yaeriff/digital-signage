a761510172ed089c706978eeb3e1f98a896a8d72
Rifqi Akmal Musyaffa <ram89291@gmail.com>
2026-02-11 11:10:24 +0700

    Add custom php.ini

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     
-    <link rel="stylesheet" href="{{ asset('signage/assets/css/stel.css') }}">
+    <link rel="stylesheet" href="{{ asset('signage/assets/css/steel.css') }}">
 
     <style>
     /* Styling Tambahan untuk Agenda Loop */
